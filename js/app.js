97378845ff6c283709941a1842831704fe597c0a
Michael Q <MichaelQuiban@outlook.com>
2016-06-26 14:04:00 -0700

    Added JQuery events and callbacks.
    When you hover over the .ryu div, you'll get an event response switching
    the images.
    Leaving the image will also fire an event which currently has an alert
    placeholder.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-$(document).ready(){
-
-}
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-stand').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    alert('mouse left');
+  });
+});
