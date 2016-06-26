480fd124d6d18d1439bc041535a017c06b7a7937
Michael Q <MichaelQuiban@outlook.com>
2016-06-26 14:57:53 -0700

    Added some working HTML inspiration from another SF project.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,36 @@
-$(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-stand').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-stand').show();
-  })
-  .mousedown(function() {
-    console.log('mousedown');
-    // play hadouken sound
-    // show hadouken and animate it to the right of the screen
-    $('.ryu-ready').hide();
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-stand').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-stand').show();
+        $('.ryu-ready').hide();
+    })
+      .mousedown(function() {
+    playHadouken();
+    $('.ryu-action').hide();  
     $('.ryu-throwing').show();
-    $('.hadouken').show();
+    $('.hadouken').finish().show()
+      .animate(
+        {'left': '300px'},
+        500,
+        function() {
+          $(this).stop();
+          $(this).hide();
+          $(this).css('left', '-212px');
+        }
+      );
   })
-  .mouseup(function() {
-    console.log('mouseup');
-   $('.ryu-throwing').hide();
-   $('.ryu-ready').show();
-  });
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
