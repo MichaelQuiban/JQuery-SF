39faf05fd02c14bca6730c51a27d5814453eeb40
Michael Q <MichaelQuiban@outlook.com>
2016-06-26 15:15:36 -0700

    Added Text and Animations.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,39 @@
-$(document).ready(function(){
-    $('.ryu').mouseenter(function(){
-        $('.ryu-stand').hide();
-        $('.ryu-ready').show();
-    })
-    .mouseleave(function() {
-        $('.ryu-stand').show();
-        $('.ryu-ready').hide();
-    })
-      .mousedown(function() {
+$(document).ready(function() {
+	doIntro();
+	playGame();
+});
+
+var hadoukenSound = false;
+function playHadouken () {
+  hadoukenSound = !hadoukenSound;
+  if (hadoukenSound) {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+}
+
+var coolSound = false;
+function playCool () {
+  coolSound = !coolSound;
+  if (coolSound) {
+    $('#theme-song')[0].pause();
+    // $('#cool')[0].load()
+    $('#cool')[0].play();
+  }
+}
+
+
+function playGame() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-action').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-action').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
     playHadouken();
     $('.ryu-action').hide();  
     $('.ryu-throwing').show();
@@ -22,15 +48,38 @@ $(document).ready(function(){
         }
       );
   })
-    .mouseup(function(){
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-    });
+  .mouseup(function() {
+    $('.ryu-action').hide();
+    $('.ryu-still').show();
+  });
 
-});
-
-function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
+  $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      playCool();
+      $('.ryu-action').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('#cool')[0].pause();
+      $('#cool')[0].load();
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
 }
+
+function doIntro() {
+  $('#theme-song')[0].volume = 0.3;
+  $('#theme-song')[0].play();
+      $('.brought-by').fadeIn(1500, function() {
+        $(this).fadeOut(1000, function() {
+          $('#').fadeIn(1500, function() {
+            $(this).fadeOut(1500, function() {
+              $('.how-to').fadeIn(1000);
+            });
+          })
+        })
+      })
+  }
+
